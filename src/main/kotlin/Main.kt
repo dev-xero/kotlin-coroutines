7ffc91c97730d3d139234581f7dabe8f6ea544d7
Xero kun <elo48858@gmail.com>
2023-02-21 17:35:24 +0100

    `✨UPDATE` Used launch to Launch Coroutines

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,29 @@
 import kotlinx.coroutines.*
+import kotlin.system.*
 
 fun main() {
-    runBlocking {
-        println("Weather Forcast")
-        printForcast()
+    val time = measureTimeMillis {
+        runBlocking {
+            println("Weather Forcast")
+            launch {
+                printForcast()
+            }
+
+            launch {
+                printForcast()
+            }
+        }
     }
+
+    println("Execution time(seconds): ${time / 1000.0}s")
 }
 
 suspend fun printForcast() {
     delay(1000)
     println("Sunny")
+}
+
+suspend fun printTemp() {
+    delay(1000)
+    println("30℃")
 }
